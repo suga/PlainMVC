a8f4042851fad1a292bf2be73f122261d1587d5c
hlegius <hlegius@yahoo.com.br>
2010-02-22 23:15:58 -0300

    Master: removing dummy files.

--- library/simplemvc/util/RepositoryException.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Repository Exception
- * @author Hélio Costa e Silva <hlegius@foobug.com.br>
- * @package \library\Plain\util
- * @version January, 23 2010
- */
-class RepositoryException extends RuntimeException { }
